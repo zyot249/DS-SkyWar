a8b8bd5014166eeac25425ffde64ed64f14fb07d
Zyot249 <zyot249@gmail.com>
2020-04-16 22:08:52 +0700

    maven project

---
+++ src/main/java/com/zyot/fung/shyn/App.java
@@ -0,0 +1,13 @@
+package com.zyot.fung.shyn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
